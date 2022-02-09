7336a42bf248a12f50705f5448180db685e90203
niludarekar1 <nilu.darekar1@gmail.com>
2022-02-09 15:23:29 +0530

    Added:LinkedList,StackandQueue

---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+class Node {
+    int data;
+    Node next;
+
+    //Parameterized Constructor to initialize Data
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
---
+++ src/com/bridgelabz/StackQueue.java
@@ -0,0 +1,43 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class StackQueue extends linkedList {
+
+    public static void main(String args[]) {
+        StackQueue stackQueue = new StackQueue();
+        System.out.println("Welcome to Stacks and Queues Program");
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        choice = sc.nextInt();
+        do {
+            System.out.println("\n 1.Push in Stack \n 2.Pop from Stack \n 3.Display Stack \n 4.Insert data in in Queue \n 5. Delete data from Queue \n 6.Quit ");
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the Number which you want to add in stack ");
+                    int number = sc.nextInt();
+                    stackQueue.insertFirst(number);
+                    break;
+                case 2:
+                    stackQueue.pop();
+                    break;
+                case 3:
+                    stackQueue.showList();
+                    break;
+                case 4:
+                    System.out.println("Enter the data which you want to add in Queue");
+                    int data = sc.nextInt();
+                    stackQueue.insertLast(data);
+                    break;
+                case 5:
+                    stackQueue.popLast();
+                    break;
+                case 6:
+                    System.out.println("Thank you");
+                    break;
+            }
+
+        } while (choice != 6);
+    }
+
+}
